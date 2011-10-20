db9df8f802aaa40aacf92ebdc3b2f8004f9cc1df
Marlena Compton <mozmarlena@gmail.com>
2011-10-20 15:47:04 -0700

    can open minibrowser

--- test_payment.py
+++ test_payment.py
@@ -19,3 +19,10 @@ class TestPayment:
         Assert.contains("Purchase Add-on", details_page.purchase_modal_title)
         details_page.click_pay_with_paypal()
         Assert.contains("Log in to your PayPal account to complete this purchase.", details_page.paypal_login_message)
+        details_page.click_paypal_login()
+        #this is where things get tricky. Clicking on the yellow login button 
+        #executes super-sekrit paypal js that nobody but paypal can see.
+        #The js opens a new window....we don't know what the id for that window is.  
+        
+        #After googling, this looks promising.  It recommends using window.open('', <name>):  
+        #http://code.google.com/p/browsersec/wiki/Part2#Gaps_in_DOM_access_control
